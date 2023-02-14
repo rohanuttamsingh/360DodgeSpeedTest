a291cab6a1b71c3aecb5131a354868c22a6a8753
Rohan Uttamsingh <rohanuttamsingh@gmail.com>
2023-02-14 17:13:40 -0500

    Better speed measure

--- speed_test.py
+++ speed_test.py
@@ -2,14 +2,14 @@
 import argparse
 import os
 import tensorflow as tf
-from models import generate_s2d_model, generate_smaller_model, generate_even_smaller_model
+from models import generate_s2d_model, generate_smaller_model, generate_even_smaller_model, generate_tiny_model
 from data import get_inputs_in_memory
-from datetime import datetime
+from time import perf_counter
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-m', '--model', type=str, help='Options: s2d | smaller | even')
+parser.add_argument('-m', '--model', type=str, help='Options: s2d | smaller | even | tiny')
 parser.add_argument('-c', '--checkpoint_path', type=str, help='Path to model checkpoint')
 parser.add_argument('-d', '--data_path', type=str, help='Path to diretory with input files')
 parser.add_argument('-n', '--normalized', action='store_true', help='Set if the dataset is normalized')
@@ -21,18 +21,20 @@ elif args.model == 'smaller':
     model = generate_smaller_model(args.normalized)
 elif args.model == 'even':
     model = generate_even_smaller_model(args.normalized)
+elif args.model == 'tiny':
+    model = generate_tiny_model(args.normalized)
 model.load_weights(args.checkpoint_path)
 
 inputs = get_inputs_in_memory(args.data_path, 'val', False, args.normalized, False)
 inputs = [tf.expand_dims(input, axis=0) for input in inputs]
 ds_size = len(inputs)
 
-start_time = datetime.now()
+start_time = perf_counter()
 for input in inputs:
     pred = model(input, training=False)
-end_time = datetime.now()
+end_time = perf_counter()
 
-total_seconds = (end_time - start_time).total_seconds()
+total_seconds = end_time - start_time
 average_seconds = total_seconds / ds_size
 average_hertz = 1 / average_seconds
 
